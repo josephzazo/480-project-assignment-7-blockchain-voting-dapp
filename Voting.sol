3fcc7c9e5ebf8bdb6cd5d9e2f044d71bd8c54ec8
Joseph Zazo <89111224+josephzazo@users.noreply.github.com>
2026-04-22 16:24:10 -0700

    Updated Voting.sol
    Added first TODO and moved struct above public variables to follow standard conventions

--- Voting.sol
+++ Voting.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+// TODO 1: Define the Proposal struct to store voting data
+
 contract Voting {
-    address public owner;
-    uint256 public proposalCount;
 
     struct Proposal {
         uint256 id;
@@ -12,3 +12,7 @@ contract Voting {
         uint256 voteCount;
         bool active;
     }
+
+    address public owner;
+    uint256 public proposalCount;
+}
